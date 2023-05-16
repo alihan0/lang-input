8b33cc650f31ac481132d8f445d659d88bf3d680
Alihan Öztürk <52991611+alihan0@users.noreply.github.com>
2023-05-16 08:05:25 +0300

    Update lang-input.js

--- src/js/lang-input.js
+++ src/js/lang-input.js
@@ -4,6 +4,9 @@
       var placeholder = options.placeholder ||  "";
       var placeholderLangPrefix = options.placeholderLangPrefix;
       var inputname = options.name || $(this).attr('id');
+      var inputType = options.input || "text";
+      var textareaRows = options.textareaRows || 0;
+      var textareaCols = options.textareaCols || 0;
       
       return this.each(function() {
         var $container = $(this);
@@ -11,8 +14,8 @@
         var $tabList = $('<ul class="nav nav-tabs" role="tablist"></ul>');
         var $tabContent = $('<div class="tab-content"></div>');
         $.each(langs, function(i, lang) {
-          var $tabLink = $('<a class="nav-link text-dark fw-bold" data-bs-toggle="tab" href="#tab-' + i + '" role="tab">' + lang + '</a>');
-          var $tabPane = $('<div class="tab-pane" id="tab-' + i + '" role="tabpanel"></div>');
+          var $tabLink = $('<a class="nav-link text-dark fw-bold" data-bs-toggle="tab" href="#tab-' + inputname + '-' + i + '" role="tab">' + lang + '</a>');
+          var $tabPane = $('<div class="tab-pane" id="tab-' + inputname + '-' + i + '" role="tabpanel"></div>');
           $tabList.append($('<li class="nav-item"></li>').append($tabLink));
           $tabContent.append($tabPane);
   
@@ -25,8 +28,8 @@
   
         // tablere içerik ekleme
         $.each(langs, function(i, lang) {
-          var $tabPane = $('#tab-' + i);
-          var $content = $container.find('.content-' + lang);
+          var $tabPane = $('#tab-' + inputname + '-' + i);
+          var $content = $container.find('.content-'+ inputname + '-' + lang);
           if ($content.length) {
             $tabPane.append($content.html());
           } else {
@@ -34,12 +37,22 @@
             var placeholderText = placeholderLangPrefix ? lang.toUpperCase() + ' ' + placeholder : placeholder;
             var input;
 
-            if(placeholderLangPrefix === true){
-                input = '<div class="input-group mb-3"><span class="input-group-text">'+lang.toUpperCase()+'</span><input type="text" class="form-control" name='+inputname+'-' + lang + ' id="'+inputname+'-' + lang + '" placeholder="'+placeholder+'"></div>'
-            }else{
-                input = '<input class="form-control type="text" name="' + inputname + '-' + lang + '" id="' + inputname + '-' + lang + '" placeholder="' + placeholderText + '"/>';
+            if(inputType == "text"){
+              if(placeholderLangPrefix === true){
+                input = '<div class="input-group mb-3"><span class="input-group-text">'+lang.toUpperCase()+'</span><input type="text" class="form-control" name='+inputname+'_' + lang + ' id="'+inputname+'_' + lang + '" placeholder="'+placeholder+'"></div>'
+              }else{
+                  input = '<input class="form-control type="text" name="' + inputname + '_' + lang + '" id="' + inputname + '_' + lang + '" placeholder="' + placeholderText + '"/>';
+              }
+
+            }else if(inputType == "textarea"){
+              if(placeholderLangPrefix === true){
+                input = '<div class="input-group mb-3"><span class="input-group-text">'+lang.toUpperCase()+'</span><textarea placeholder="'+placeholderText+'" class="form-control" cols="'+textareaCols+'" rows="'+textareaRows+'" id="'+inputname+'_' +lang+'" name="'+inputname+'_'+lang+'"></textarea></div>'
+              }else{
+                  input = '<textarea placeholder="'+placeholderText+'" class="form-control" cols="'+textareaCols+'" rows="'+textareaRows+'" id="'+inputname+'_'+lang+'" name="'+inputname+'_'+lang+'"></textarea>';
+              }
             }
 
+          
             
 
 
